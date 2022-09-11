cb6f010664d44672374e103672a63b370d6e481e
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-09-10 22:48:41 -0700

    initial commit mark 2

--- ParksAPI/Program.cs
+++ ParksAPI/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Parks
+namespace ParksAPI
 {
   public class Program
   {
